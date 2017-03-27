1103f91a008b0362efffb4c1d00e28cdda09c45c
Leah Hirst <leah@hirst.dev>
2017-03-27 17:24:04 +0100

    Added event scrapper

--- getEventDetail.js
+++ getEventDetail.js
@@ -2,11 +2,22 @@
  * Returns details about the event
  */
 function getEventDetail() {
-  // TODO: Implement
+  // Scrap the attributes from the page
+  var title = $("h1.listing-hero-title")[0].innerHTML;
+  var image = $("img.listing-hero-image").parent()[0].getAttribute("content");
+
+  // Wrap it in a single object
+  var eventDetail = {
+    title: title,
+    image: image
+  }
+
+  // Return the details
+  return eventDetail;
 }
 
 // Register the action with Chrome
 chrome.runtime.sendMessage({
   action: "getEventDetail",
-  source: getEventDetail()
+  eventDetail: getEventDetail()
 });
